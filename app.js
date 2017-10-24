c215cdc24cbae744fbf90204aec57345ffc2ad02
owhc <owenc.au@gmail.com>
2017-10-24 15:08:03 +0800

    appdemo init

---
+++ app.js
@@ -0,0 +1,107 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+var routes = require('./routes/control');
+
+// DataBase 
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "dbadmin",
+    password: "%TGBmju7",
+    database: "ibmclouddb"
+});
+con.connect(function(err) {
+  if (err) {
+      console.log('connecting error');
+      return;
+  }
+  console.log('connecting success');
+});
+
+
+// create a new express server
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// db state
+app.use(function(req, res, next) {
+    req.con = con;
+    next();
+});
+
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+// // serve the files out of ./public as our main files
+// app.use(express.static(__dirname + '/public'));
+
+// // get the app environment from Cloud Foundry
+// var appEnv = cfenv.getAppEnv();
+
+// // start server on the specified port and binding host
+// app.listen(appEnv.port, '0.0.0.0', function() {
+//   // print a message when the server starts listening
+//   console.log("server starting on " + appEnv.url);
+// });
---
+++ routes/control.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+
+// home page
+router.get('/', function(req, res, next) {
+    
+        var db = req.con;
+        var data = "";
+    
+        db.query('SELECT * FROM datacenter', function(err, rows) {
+            if (err) {
+                console.log(err);
+            }
+            var data = rows;
+    
+            // use index.ejs
+            res.render('index', { title: 'Datacenter Location', data: data});
+        });
+    
+    });
+
+module.exports = router;
