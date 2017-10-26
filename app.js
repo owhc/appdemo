1352e8b130f6c68646796c22bef043977e104fa1
owhc <owenc.au@gmail.com>
2017-10-26 21:22:07 +0800

    mysql RDS internal url update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var routes = require('./routes/control');
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
-    host: "rm-3nsr0u2pou399ja4no.mysql.rds.aliyuncs.com",
+    host: "rm-3nsr0u2pou399ja4n.mysql.rds.aliyuncs.com",
     user: "dbadmin",
     password: "%TGBmju7",
     database: "ibmclouddb"
